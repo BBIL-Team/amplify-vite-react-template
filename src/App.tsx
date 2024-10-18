fdcbbe8840b846342e3bc2af4bd26b8295d0f2e4
BBIL-Team <deepshika5686@bharatbiotech.com>
2024-10-18 16:26:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-interface AppProps {
-    user: { username: string };
-    signOut: () => void;
-}
-
 const App: React.FC<AppProps> = ({ user, signOut }) => {
+     const handleFetchTasks = (event) => {
+        // Conditionally check for event before using preventDefault
+        if (event) {
+            event.preventDefault();
+        }
+
+        fetch(`https://imf44ag3d4.execute-api.ap-south-1.amazonaws.com/S1/Test5?EmployeeID=${encodeURIComponent(employeeID)}`)
+            .then(response => response.text())
+            .then(data => {
+                const taskRows = parseTasksData(data);
+                setTasks(taskRows);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setTasks([]);
+                alert('Error fetching data. Please try again later.');
+            });
+    };
     return (
         <div>
             <header>
