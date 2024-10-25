f94aa1b0692865c6cf2031c56cd8ebe5a7d1078d
BBIL-Team <deepshika5686@bharatbiotech.com>
2024-10-25 11:47:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -120,7 +120,7 @@ function App() {
 
 
   return (
-    <div style={{ height: '720px', display: 'flex', flexDirection: 'column', padding: '10', width: '80vw', margin: '0 auto', boxSizing: 'border-box',backgroundcolor: #EBEBEB; }}>
+    <div style={{ height: '720px', display: 'flex', flexDirection: 'column', padding: '10', width: '80vw', margin: '0 auto', boxSizing: 'border-box',backgroundcolor: '#EBEBEB'; }}>
       <header>
         <img src="https://www.bharatbiotech.com/images/bharat-biotech-logo.jpg" alt="Company Logo" className="logo" />
       </header>
